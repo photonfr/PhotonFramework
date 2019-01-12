75ffe1c3f20220e4e9e8c38fec0ee41596eda2a8
NuttsDev <42683134+NuttsDev@users.noreply.github.com>
2019-01-11 22:00:00 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var lines = []
+var line = 0;
+var addingLines = 1;
+if(addingLines == 1) {
+	var code = prompt("Enter code to use. (Enter nothing to stop)");
+	line++;
+}
+if(code == "") {
+	addingLines = 0;
+	
+} else {
+	document.write("LINE " + line + ":" + code)
+	lines.push(code)
+}
